335c5509884f80fa27b05cb9a4d8b0f123d641f0
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-06-23 09:59:55 -0500

    Comienzo el proyecto con angular material

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  forms:FormGroup;
+  loading:boolean = false;
+
+  constructor(private fb:FormBuilder, private _snackBar: MatSnackBar){
+    this.forms = this.fb.group({
+      usuario: ["", Validators.required],
+      password: ["", [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  ingresar(){
+    const USUARIO = this.forms.value.usuario; 
+    const PASSWORD = this.forms.value.password; 
+
+    if (USUARIO == "santi123" && PASSWORD == "admin123") {
+      // Redireccionamos al Dashboard
+      this.fakeLoading();
+    } else{
+      this.error();
+      this.forms.reset();
+    }
+  }
+
+  error(){
+    this._snackBar.open('Usuario o contraseña ingresado son invalidos', "", {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  };
+  
+  fakeLoading(){
+    this.loading = true;
+    setTimeout(()=>{
+      this.loading = false;
+      // Redireccionamos al Dashboard
+    },1500)
+  }
+
+}
---
+++ src/app/components/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Angular Material
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+//Modulos
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class SharedModule { }
